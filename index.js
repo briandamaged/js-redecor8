dee70e80c80811f5f818e1be8bd035b2efae61d8
Brian Lauber <brian.lauber@33across.com>
2019-01-29 20:34:33 -0500

    mv LogsErrors.js -> logging/LogsErrors.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ Object.assign(exports, {
   AppliesWhen: require('./redecor8/AppliesWhen'),
   compose: require('./redecor8/compose'),
   InstallsWhen: require('./redecor8/InstallsWhen'),
-  LogsErrors: require('./redecor8/LogsErrors'),
+  LogsErrors: require('./redecor8/logging/LogsErrors'),
   Occasionally: require('./redecor8/Occasionally'),
   OnErrorReturns: require('./redecor8/OnErrorReturns'),
   Pass: require('./redecor8/Pass'),
---
+++ redecor8/logging/index.js
@@ -0,0 +1,4 @@
+
+Object.assign(exports, {
+  LogsErrors: require('./LogsErrors'),
+});
